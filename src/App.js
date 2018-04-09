5f373c796aebdcbee083671d1a6e295578c76705
quan <quanluis@protonmail.com>
2018-04-09 18:56:37 -0400

    Created a Sign in component and linked it into app.js with react router.

--- src/App.js
+++ src/App.js
@@ -6,20 +6,27 @@ import { Route, Link } from 'react-router-dom';
 import Home from './Pages/Home';
 import Profile from './Pages/Profile'; 
 import Signup from './Pages/Signup';
+import SignIn from './Pages/SignIn'
 
 class App extends Component {
   render() {
     return (
       <div>
+        
         <li>
           <Link to = '/profile' > Profile </Link> 
         </li>
         <li>
           <Link to = '/signup' > Signup </Link>
         </li>
+        <li>
+          <Link to = '/signin' > Signin </Link>
+        </li>
+        <h1>Catbook</h1>
         <Route path = '/' exact Component = {Home} />
-        <Route path = '/profile' component = {Profile} />
-        <Route path = '/signup' component = {Signup} /> 
+        <Route path = '/profile' exact component = {Profile} />
+        <Route path = '/signup' exact component = {Signup} /> 
+        <Route path = '/signin' exact component = {SignIn} />
       </div>
     );
   }
---
+++ src/Pages/SignIn.js
@@ -0,0 +1,8 @@
+import React from 'react';
+
+const Signin = props => (
+    <div>
+        
+    </div>
+)
+export default Signin; 
